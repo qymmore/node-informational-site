85743f74b7e862e5b7962c28fb15cd96ae2f4a65
Sarah Quayyum <95451775+qymmore@users.noreply.github.com>
2022-11-09 19:12:31 -0500

    refactor code using expressJS

--- index.js
+++ index.js
@@ -1,34 +1,68 @@
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
+
 const path = require('path');
-
-const server = http.createServer((req,res) => {
-    if(req.url === '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            if(err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
-        })
-    } else if(req.url === '/about') {
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-            if(err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
-        })
-    } else if(req.url === '/contact') {
-        fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, content) => {
-            if(err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
-        })
-    } else {
-        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-            if(err) throw err;
-            res.writeHead(500, {'Content-Type': 'text/html'});
-            res.end(content);
-        })}
-});
-
+const express = require('express');
+const app = express();
+const router = express.Router();
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, () => console.log(`Server running on PORT ${PORT}...`));
+const Logger = (req,res,next) => {
+    console.log("Request IP: " + req.ip);
+    console.log("Request Method: " + req.method);
+    console.log("Request date: " + new Date());
+
+    next();
+}
+
+app.use(Logger);
+
+router.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+router.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+})
+
+router.get('/contact', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+})
+
+router.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', '404.html'));
+})
+
+app.use('/', router);
+
+app.listen(process.env.PORT || 8080);
+console.log(`Server running on port: ${PORT}`);
+
+// const server = http.createServer((req,res) => {
+//     if(req.url === '/') {
+//         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+//             if(err) throw err;
+//             res.writeHead(200, {'Content-Type': 'text/html'});
+//             res.end(content);
+//         })
+//     } else if(req.url === '/about') {
+//         fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+//             if(err) throw err;
+//             res.writeHead(200, {'Content-Type': 'text/html'});
+//             res.end(content);
+//         })
+//     } else if(req.url === '/contact') {
+//         fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, content) => {
+//             if(err) throw err;
+//             res.writeHead(200, {'Content-Type': 'text/html'});
+//             res.end(content);
+//         })
+//     } else {
+//         fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+//             if(err) throw err;
+//             res.writeHead(500, {'Content-Type': 'text/html'});
+//             res.end(content);
+//         })}
+// });
+
+// server.listen(PORT, () => console.log(`Server running on PORT ${PORT}...`));
